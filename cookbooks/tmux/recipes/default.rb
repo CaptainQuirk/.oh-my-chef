8b59703dea570d204fb71bdc892b6769e3684849
lmessier <leonard.messier@gmail.com>
2013-05-21 14:01:35 +0200

    [feature] Replacing template by git provider

--- cookbooks/tmux/recipes/default.rb
+++ cookbooks/tmux/recipes/default.rb
@@ -2,6 +2,15 @@
 package 'tmux'
 
 # Loading template
-template "#{ENV['HOME']}/.tmux.conf" do
-    source "tmux.conf.erb"
+#template "#{ENV['HOME']}/.tmux.conf" do
+    #source "tmux.conf.erb"
+#end
+
+# Downloading tmux configuration in hidden directory
+git "~/.tmux" do
+  repository "https://github.com/CaptainQuirk/.tmux.git"
+  reference "master"
+  action :sync
 end
+
+# Symlinking this config file
