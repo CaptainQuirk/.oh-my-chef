09213b122f57e360bd774efce4f6b416e67adf12
lmessier <leonard.messier@gmail.com>
2013-05-21 16:15:58 +0200

    [bug] Home directory doesn't expand

--- cookbooks/tmux/recipes/default.rb
+++ cookbooks/tmux/recipes/default.rb
@@ -7,14 +7,14 @@ package 'tmux'
 #end
 
 # Downloading tmux configuration in hidden directory
-git "~/.tmux" do
+git "#{ENV['HOME']}/.tmux" do
   repository "https://github.com/CaptainQuirk/.tmux.git"
   reference "master"
   action :sync
 end
 
 # Symlinking this config file
-link "~/.tmux.conf" do
-  to "~/.tmux/tmux.conf"
+link "#{ENV['HOME']}/.tmux.conf" do
+  to "#{ENV['HOME']}/.tmux/tmux.conf"
   action :create
 end
