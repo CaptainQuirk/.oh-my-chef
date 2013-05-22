6f151e90340dc00f5ecf78b2b1cb539a8a141dda
lmessier <leonard.messier@gmail.com>
2013-05-22 18:46:20 +0200

    [feature] Adding zsh recipe

---
+++ cookbooks/zsh/recipes/default.rb
@@ -0,0 +1,22 @@
+# Installing tmux package
+package 'zsh'
+
+# Downloading tmux configuration in hidden directory
+git "#{ENV['HOME']}/.zsh" do
+  repository "https://github.com/CaptainQuirk/.zsh.git"
+  reference "master"
+  enable_submodules true
+  action :sync
+end
+
+# Symlinking this config file
+link "#{ENV['HOME']}/.zsh" do
+  to "#{ENV['HOME']}/.zsh/zshrc"
+  action :create
+end
+
+# Changing default shell
+bash "change_default_shell" do
+  cwd "#{ENV['HOME']}"
+  command "chsh -s /bin/zsh"
+end
