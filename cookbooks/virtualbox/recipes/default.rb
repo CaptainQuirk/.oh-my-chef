758b571d95a4f4179433f3bd0cdf686a99ce84d8
lmessier <leonard.messier@gmail.com>
2013-05-27 14:23:02 +0200

    [feature] Downloading ievms script repo

--- cookbooks/virtualbox/recipes/default.rb
+++ cookbooks/virtualbox/recipes/default.rb
@@ -1,2 +1,11 @@
 # Installing virtualbox package
 package 'virtualbox'
+
+# Cloning ievms project
+git "#{ENV['HOME']}/.iemvs-script" do
+  repository "https://github.com/xdissent/ievms.git"
+  reference "master"
+  action :sync
+end
+
+# New values in the crontab
