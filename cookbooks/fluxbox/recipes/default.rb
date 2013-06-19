34e6cafb55cb49e7cd5574dc95e6214ba225d89f
lmessier <leonard.messier@gmail.com>
2013-06-19 14:01:49 +0200

    Deleting files

--- cookbooks/fluxbox/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-package 'fluxbox'
-
-git "#{ENV['HOME']}/.fluxbox" do
-  repository "https://github.com/CaptainQuirk/.fluxbox.git"
-  reference "master"
-  enable_submodules true
-  action :sync
-end
