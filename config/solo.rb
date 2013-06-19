7e0c3d6ce3ac867fa2f12a1b360e3e6bbe26f497
lmessier <leonard.messier@gmail.com>
2013-06-19 14:06:56 +0200

    [bug] Adding slash character to cookbooks paths

--- config/solo.rb
+++ config/solo.rb
@@ -1,7 +1,7 @@
 root_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
  
 base_cookbook_path = File.join(root_path, 'cookbooks')
-cookbook_path [ "#{base_cookbook_path}commands", "#{base_cookbook_path}configuration", "#{base_cookbook_path}dev", "#{base_cookbook_path}graphics", "#{base_cookbook_path}libs", "#{base_cookbook_path}media", "#{base_cookbook_path}network", "#{base_cookbook_path}system" ]
+cookbook_path [ "#{base_cookbook_path}/commands", "#{base_cookbook_path}/configuration", "#{base_cookbook_path}/dev", "#{base_cookbook_path}/graphics", "#{base_cookbook_path}/libs", "#{base_cookbook_path}/media", "#{base_cookbook_path}/network", "#{base_cookbook_path}/system" ]
 role_path       File.join(root_path, 'roles')
  
 # Move all the state stuff from /var/chef. I wish there was a single config
