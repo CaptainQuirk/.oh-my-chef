e06c4bfebb90c3bcc4a9dc7176c38e68eeafcbfd
lmessier <leonard.messier@gmail.com>
2013-06-24 09:47:42 +0200

    [feature] transmuter gem

---
+++ cookbooks/commands/transmuter/recipes/default.rb
@@ -0,0 +1,12 @@
+# Paquet python
+package 'python-pygments'
+
+# xml dev libraries
+package 'libxslt-dev'
+package 'libxml2-dev'
+
+# Wkhtmltopdf
+package 'wkhtmltopdf'
+
+# gem
+gem_package 'transmuter'
