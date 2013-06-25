6548b342207c75c2ba8b83fefa0b7a088c15e789
lmessier <leonard.messier@gmail.com>
2013-06-25 14:14:24 +0200

    [feature] Appcelerator cookbook

---
+++ cookbooks/dev/appcelerator/recipes/default.rb
@@ -0,0 +1,18 @@
+# Installing oracle's java dev kit
+package 'default-jdk'
+
+
+# Installing android sdk
+# ----------------------
+
+# Getting remote archive
+
+# check md5 sum
+
+# create .android folder
+
+# move the archive to it and untar it
+
+# Adding a shortcut in ~/bin folder for program tools/android
+
+# Launching the update command for the sdk
