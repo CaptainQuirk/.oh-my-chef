bb53f4dd726c72c774f0f99e8b9a02d6b68b6532
lmessier <leonard.messier@gmail.com>
2013-06-28 14:17:31 +0200

    [feature] Starting implementing appcelerator cookbook
    * using attribute file to store some values
    * getting remote archive file, checsumming it
    * untaring the archive
    * launching sdk update command

---
+++ cookbooks/dev/appcelerator/attributes/default.rb
@@ -0,0 +1,8 @@
+
+# Node variables for android sdk
+# ------------------------------
+
+default['android-sdk']['filename']     = "android-sdk_r22.0.1-linux.tgz"
+default['android-sdk']['base_url']     = "http://dl.google.com/android"
+default['android-sdk']['checksum']     = "56ed27d456b4f0e0d3090b24f9b06757"
+default['android-sdk']['extract_path'] = "#{ENV['home']}/.android/android-sdk"
--- cookbooks/dev/appcelerator/recipes/default.rb
+++ cookbooks/dev/appcelerator/recipes/default.rb
@@ -5,14 +5,42 @@ package 'default-jdk'
 # Installing android sdk
 # ----------------------
 
+# Extracting attribute values in attributes/default.rb
+src_filename = "#{node['android-sdk']['filename']}"
+src_url      = "#{node['android-sdk']['base_url']}/#{src_filename}"
+src_checksum = "#{node['android-sdk']['checksum']}"
+src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filname}"
+extract_path = "#{ENV['home']}/.android/android-sdk"
+
 # Getting remote archive
+remote_file src_filepath do
+  source src_url 
+  checksum src_checksum 
+end
 
-# check md5 sum
-
-# create .android folder
-
-# move the archive to it and untar it
+bash 'extract_sdk' do
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+    mkdir -p #{extract_path}
+    tar xzf #{src_filename} -C #{extract_path}
+    mv #{extract_path}/*/* #{extract_path}/
+  EOH
+  not_if { ::File.exists?(extract_path) }
+end
 
 # Adding a shortcut in ~/bin folder for program tools/android
 
-# Launching the update command for the sdk
+# Launching the update command for the sdk tools/android sdk -u
+bash 'update sdk' do
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+    ./tools/android sdk -u
+  EOH
+  not_if { ::File.exists?(extract_path) }
+end
+
+# Installing npm ?
+
+# Connecting to appcelerator's account to download sdk if it
+# wasn't done
+
