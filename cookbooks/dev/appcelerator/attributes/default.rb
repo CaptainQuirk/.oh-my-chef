c39910713ce021acca71c6f223ddbbfb097c58e8
lmessier <leonard.messier@gmail.com>
2013-06-29 19:29:55 +0200

    [bug] fixing HOME env variable, again ...

--- cookbooks/dev/appcelerator/attributes/default.rb
+++ cookbooks/dev/appcelerator/attributes/default.rb
@@ -5,5 +5,5 @@
 default['android-sdk']['filename']     = "android-sdk_r22.0.1-linux.tgz"
 default['android-sdk']['base_url']     = "http://dl.google.com/android"
 default['android-sdk']['checksum']     = "56ed27d456b4f0e0d3090b24f9b06757"
-default['android-sdk']['extract_path'] = "#{ENV['home']}/.android/android-sdk"
-default['android-sdk']['bin_symlink']  = "#{ENV['home']}/bin/android"
+default['android-sdk']['extract_path'] = "#{ENV['HOME']}/.android/android-sdk"
+default['android-sdk']['bin_symlink']  = "#{ENV['HOME']}/bin/android"
