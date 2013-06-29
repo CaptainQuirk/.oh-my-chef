3d4e6855451fc9ac9966ead1e996a73c4162d53b
lmessier <leonard.messier@gmail.com>
2013-06-29 19:26:04 +0200

    [feature] symlinking android sdk executable

--- cookbooks/dev/appcelerator/recipes/default.rb
+++ cookbooks/dev/appcelerator/recipes/default.rb
@@ -11,6 +11,7 @@ src_url      = "#{node['android-sdk']['base_url']}/#{src_filename}"
 src_checksum = "#{node['android-sdk']['checksum']}"
 src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}"
 extract_path = "#{ENV['HOME']}/.android/android-sdk"
+bin_symlink  = "#{node['android-sdk']['bin_symlink']}"
 
 # Getting remote archive
 remote_file src_filepath do
@@ -30,7 +31,12 @@ bash 'extract_sdk' do
   not_if { ::File.exists?(extract_path) }
 end
 
-# Adding a shortcut in ~/bin folder for program tools/android
+# Adding a symlink in ~/bin folder for program tools/android
+link bin_symlink do
+  to "#{extract_path}/tools/android"
+  action :create
+  not_if { ::File.exists?(bin_symlink) }
+end
 
 # Launching the update command for the sdk tools/android sdk -u
 bash 'update sdk' do
