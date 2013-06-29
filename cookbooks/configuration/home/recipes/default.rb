a27ee2bf5a8b9d46960fbc880ca2d217ce530589
lmessier <leonard.messier@gmail.com>
2013-06-29 22:17:06 +0200

    [bug] weird commit

--- cookbooks/configuration/home/recipes/default.rb
+++ cookbooks/configuration/home/recipes/default.rb
@@ -9,15 +9,6 @@ directory "#{ENV['HOME']}/bin" do
   action :create
 end
 
-# Adding some base bash commands
-#file "upgrade-android-sdk" do
-#  content "gnome-terminal -x sudo android update sdk -u"
-#  owner ENV['SUDO_USER']
-#  mode 0755
-#  path "#{ENV['HOME']}/bin/upgrade-android-sdk"
-#  action :create
-#end
-
 # Creating a tmp directory if it doesn't exist
 directory "#{ENV['HOME']}/tmp" do
   owner ENV['SUDO_USER']
