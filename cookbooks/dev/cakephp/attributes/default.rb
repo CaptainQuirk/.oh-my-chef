c958e943350ada184238012e9c052b8a510d4b96
lmessier <leonard.messier@gmail.com>
2013-07-09 23:14:16 +0200

    [feature] Attributes for default recipe

---
+++ cookbooks/dev/cakephp/attributes/default.rb
@@ -0,0 +1,7 @@
+default['cakephp']['repository']          = "https://github.com/cakephp/cakephp.git"
+default['cakephp']['path']                = ".cakephp"
+default['cakephp']['local_repo_name']     = "cakephp"
+default['cakephp']['split_branch']        = "split-cakelib"
+default['cakephp']['subtree_origin_name'] = "split-lib"
+default['cakephp']['subtree_repo_name']   = "lib.git"
+
