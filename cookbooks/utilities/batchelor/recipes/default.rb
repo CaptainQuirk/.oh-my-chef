e8df19fa7d6e007a37ab05753e00b1968b75c6e4
lmessier <leonard.messier@gmail.com>
2013-08-18 23:34:36 +0200

    [feature] Downloading repository

--- cookbooks/utilities/batchelor/recipes/default.rb
+++ cookbooks/utilities/batchelor/recipes/default.rb
@@ -1,3 +1,12 @@
+
+# Downloading batchelor configuration in hidden directory
+git "#{ENV['HOME']}/.batchelor" do
+  repository "https://github.com/CaptainQuirk/.batchelor/.git"
+  reference "master"
+  enable_submodules true
+  action :sync
+end
+
 # Launching command
 butler_launch 'welcome' do
   command 'nano'
