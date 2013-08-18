7aa3a16cb952c8ba9097dea96e8a7323520ca5b2
lmessier <leonard.messier@gmail.com>
2013-08-19 00:13:37 +0200

    [bug] fixing mistyped repo reference

--- cookbooks/utilities/batchelor/recipes/default.rb
+++ cookbooks/utilities/batchelor/recipes/default.rb
@@ -1,14 +1,14 @@
 
 # Downloading batchelor configuration in hidden directory
 git "#{ENV['HOME']}/.batchelor" do
-  repository "https://github.com/CaptainQuirk/.batchelor/.git"
+  repository "https://github.com/CaptainQuirk/.batchelor.git"
   reference "master"
   enable_submodules true
   action :sync
 end
 
 # Launching command
-butler_launch 'welcome' do
-  command 'nano'
-  action :launch
-end
+#butler_launch 'welcome' do
+  #command 'nano'
+  #action :launch
+#end
