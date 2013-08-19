a5c64ed619eff00369d0fc760b9d41033b07e26c
lmessier <leonard.messier@gmail.com>
2013-08-19 14:18:16 +0200

    [bug] setting user and group for cloned repo dir

--- cookbooks/utilities/batchelor/recipes/default.rb
+++ cookbooks/utilities/batchelor/recipes/default.rb
@@ -3,6 +3,8 @@
 git "#{ENV['HOME']}/.batchelor" do
   repository "https://github.com/CaptainQuirk/.batchelor.git"
   reference "master"
+  user ENV['SUDO_USER']
+  group ENV['SUDO_USER']
   enable_submodules true
   action :sync
 end
