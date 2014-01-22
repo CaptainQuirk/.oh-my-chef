d4cd69ffeaa262ba255f8227eafc0f87f59f65c0
lmessier <leonard.messier@gmail.com>
2014-01-22 13:57:35 +0100

    [-][clean]

--- cookbooks/fluxbox/spec/default_spec.rb
+++ cookbooks/fluxbox/spec/default_spec.rb
@@ -6,10 +6,4 @@ describe 'fluxbox::default' do
   it 'installs the fluxbox package' do
     expect(chef_run).to install_package('fluxbox')
   end
-
-  #it 'adds a line to .xinitrc to start fluxbox automatically' do
-    #expect(chef_run).to create_file_if_missing("#{ENV['USER']}/.xinitrc").with(
-      #action: :create_if_missing
-    #)
-  #end
 end
