9c57e990a91daf8e1c0f56b768155c4a4c375335
lmessier <leonard.messier@gmail.com>
2014-01-22 14:05:32 +0100

    [+] Updating fluxbox directory through git

--- cookbooks/fluxbox/recipes/default.rb
+++ cookbooks/fluxbox/recipes/default.rb
@@ -1,4 +1,10 @@
 # Install the fluxbox package
 package 'fluxbox'
 
-# Create an .xinitrc file if missing
+# Syncing fluxbox conf directory through git
+git "#{ENV['HOME']}/.fluxbox" do
+  repository "https://github.com/CaptainQuirk/.fluxbox.git"
+  reference "master"
+  enable_submodules true
+  action :sync
+end
--- cookbooks/fluxbox/spec/default_spec.rb
+++ cookbooks/fluxbox/spec/default_spec.rb
@@ -6,4 +6,12 @@ describe 'fluxbox::default' do
   it 'installs the fluxbox package' do
     expect(chef_run).to install_package('fluxbox')
   end
+
+  it 'updates the .fluxbox directory through git' do
+    expect(chef_run).to sync_git("#{ENV['HOME']}/.fluxbox").with(
+      repository: "https://github.com/CaptainQuirk/.fluxbox.git",
+      reference: "master",
+      enable_submodules: true,
+    )
+  end
 end
