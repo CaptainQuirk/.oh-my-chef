15f401372b1495a0d4ae6e273e250c2bfcd24410
lmessier <leonard.messier@gmail.com>
2014-01-30 13:52:30 +0100

    [+] Loading sudo cookbook

--- cookbooks/system/spec/default_spec.rb
+++ cookbooks/system/spec/default_spec.rb
@@ -3,6 +3,10 @@ require 'chefspec'
 describe 'system::default' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
+  it 'includes the sudo recipe' do
+    expect(chef_run).to include_recipe('sudo::default')
+  end
+
   it 'includes the grub recipe' do
     expect(chef_run).to include_recipe('grub::default')
   end
